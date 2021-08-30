383cbaa06f5cdbd2cf0808b53435844f13b5a3e0
Tristan <trungupdate@gmail.com>
2021-08-31 00:29:14 +0700

    update add

--- packages/hardhat/scripts/mint.js
+++ packages/hardhat/scripts/mint.js
@@ -12,7 +12,7 @@ const delayMS = 1000 //sometimes xDAI needs a 6000ms break lol 😅
 const main = async () => {
 
   // ADDRESS TO MINT TO:
-  const toAddress = "0x744222844bFeCC77156297a6427B5876A6769e19"
+  const toAddress = "0x77a450d9Dbe4BeE6303c6822f6E9701077a89b68"
 
   console.log("\n\n 🎫 Minting to "+toAddress+"...\n");
 
